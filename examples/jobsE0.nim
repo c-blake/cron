daf692da87e1781fe5bfe8528c037463be5c7094
Charles Blake <charlechaud@gmail.com>
2023-04-06 07:42:40 -0400

    Add `exec` and drop no longer needed `&`. Also improve NYE IO redirect.

--- examples/jobsE0.nim
+++ examples/jobsE0.nim
@@ -14,12 +14,12 @@ loop y,mo,d, h,m, w:                             # Idents can be changed
   Do("mFile"): writeFile "/tmp/m." & $m.int, ""  # Arbitrary Nim code every min
   J m.int mod 15 == 7: "every-15-on-the-7"       # 4 times/hour
   J   (h,m) ==      (0.H, 0.M): "train to GA"    # Daily
-  J (w,h,m) == (Sat, 3.H, 1.M): "fstrim /"       # Keep flash mem writes fast
+  J (w,h,m) == (Sat, 3.H, 1.M): "exec fstrim /"  # Keep flash mem writes fast
   J (w,h,m) == (Sat, 3.H,55.M): "xfs_fsr /dev/X" # Keep XFS writes on HDDs fast
   J (d,h,m) == (1.D, 4.H, 1.M): "emaint sync -a" # Monthly pull from G2
   if h.int mod 4 == 0 and m == 9.M: r "something" # Every 4th hr on the 9th min
   if (mo,d,h,m) == (Jan,1.D,0.H,0.M):
-    run "HappyNY </dev/null>>/var/log/HappyNY &", "Annual Log Job Name"
+    run "HappyNY <"&n&">/var/log/HappyNY 2>&1", "Annual Log Job Name"
 
 # If you have a holiday/business day lib/file then you could import that & do:
 #   J bizDayKind.isa(y,mo,d) and (h,m)==(0.H,9.M): "myJob"
