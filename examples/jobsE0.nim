acdc5edf13c8eb3085b95a6d0e94e5163c7832b6
Charles Blake <charlechaud@gmail.com>
2023-04-18 03:57:38 -0400

    Minor cosmetics in example.

--- examples/jobsE0.nim
+++ examples/jobsE0.nim
@@ -8,7 +8,7 @@ putEnv "PATH", HOME & "/bin:/usr/local/bin:/usr/sbin:/sbin:/usr/bin:/bin"
 cron.utc    =  false # true=>UTC tm; Use any lib you like to convert test conds
 cron.period = 60*sec # Be careful conditions are about round multiples of this!
 cron.jitter =  3*sec # Make sleep jitter 3 seconds; 0 is valid but risks herds
-cron.tmFmt  =  "%Y/%m/%d-%H:%M:%S %Z: " # Change tm format for log (note space)
+cron.tmFmt  = "%Y/%m/%d-%H:%M:%S %Z: "  # Change tm format for log (note space)
 
 # WHEN,WHAT&WHY cron.loop (which runs EVERY MINUTE) (mostly) sorted by frequency
 # In USA [1,2)am reRun;[2,3)am skip => DO NOT USE [1,3)am if Sun in Mar|Nov
@@ -17,14 +17,14 @@ loop y,mo,d, h,m,s, ns, w:                       # Idents can be changed
   Do("tmpF"): writeFile "/tmp/m." & $m.int, ""   # Backgd Nim code each period
   J   (h,m) ==     ( 0.H, 2.M): rdateSet         # Desync if in||to not Bug NIST
   sysly mo,d, h,m, w                             # Sys Mly/Wly/Dly jobs FOR ROOT
-  J m mod 15.M == 7: "every-15-on-the-7"         # 4 times/hour
+  J m mod 15.M == 7: "every-15-at-7"             # 4 times/hour
   J   (h,m) ==      (0.H, 0.M): "train to GA"    # Daily
   J (w,h,m) == (Sat, 3.H, 1.M): "exec fstrim /"  # Keep flash mem writes fast
   J (w,h,m) == (Sat, 3.H,55.M): "xfs_fsr /dev/X" # Keep XFS writes on HDDs fast
   J (d,h,m) == (1.D, 4.H, 1.M): "emaint sync -a" # Monthly pull from G2
   if h mod 4.H == 0 and m == 9.M: r "something"  # Every 4th hr on the 9th min
   if (mo,d,h,m) == (Jan,1.D,0.H,0.M):
-    run "HappyNY <"&n&">/var/log/HappyNY 2>&1", "Annual Log Job Name"
+    run "HappyNY <"&n&">/var/log/HappyNY 2>&1", "Annual Job Log Name"
   if s == 30.S:
     Do("WILL NEVER RUN"): discard # *unless* you change period to 30*sec!
 
